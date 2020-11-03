3cf14b2b9d92df979f53f142d9c425e543994ca0
Milia Anifieieva <milia.menzatova@gmail.com>
2020-11-03 01:40:03 -0500

    CanDonateTo method done

--- BloodType.cs
+++ BloodType.cs
@@ -1,8 +1,22 @@
+using System.Collections.Generic;
 namespace BloodClinic
 {
     public class BloodType
     {
-        public BloodType(string letter, char sign) {
+        private static Dictionary<string, List<string>> BloodMatrix = new Dictionary<string, List<string>>
+        {
+            {"AB+", new List<string> { "O-", "O+", "B-", "B+", "A-", "A+", "AB-", "AB+" }},
+            {"AB-", new List<string> { "O-", "B-", "A-", "AB-" }},
+            { "A+", new List<string> { "O-", "O+", "A-", "A+" }},
+            { "A-", new List<string> { "O-", "A-" }},
+            { "B+", new List<string> { "O-", "O+", "B-", "B+" }},
+            { "B-", new List<string> { "O-", "B-" }},
+            { "O+", new List<string> { "O-", "O+" }},
+            { "O-", new List<string> { "O-"} }
+        };
+
+        public BloodType(string letter, char sign)
+        {
             Letter = letter;
             Sign = sign;
         }
@@ -11,8 +25,11 @@ namespace BloodClinic
         public char Sign { get; set; } // -/+
 
         // TODO: Implement this
-        public bool CanDonateTo(BloodType otherBloodType) {
-            return true;
+        public bool CanDonateTo(BloodType recipientType)
+        {
+            var group = recipientType.Letter + recipientType.Sign;
+            List<string> types = BloodMatrix[group];
+            return types != null && types.Contains(this.Letter + this.Sign);
         }
     }
 }
